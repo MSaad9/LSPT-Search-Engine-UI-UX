a0bdbb51340f8fb323fa552a2e1464d1fc767300
Sami Fawcett <samif7466@afsenyc.org>
2021-11-06 11:15:32 -0400

    querying types populated

--- client/src/services/querying-api.types.ts
+++ client/src/services/querying-api.types.ts
@@ -1,12 +1,24 @@
 // Based on return format of Querying API response
 export interface QueryResult {
-    
+    error?: string[]
+    metrics: ResultMetrics;
+    offset: number;
+    query_display: string;
+    ac_suggest?: string[];
+    qid: number;
+    results: ResultEntry[];
 }
 
 export interface ResultEntry {
-
+    url:string;
+    title:string;
+    snippet?: string;
+    last_updated?: Date;
 }
 
 export interface ResultMetrics {
-    
+     url: string;
+     title: string;
+     snippet?: string;
+     last_updated?: Date;
 }
